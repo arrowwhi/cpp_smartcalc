a20ac09f1f60cae863534ee66d0a803c3474da75
Иван Захаров <ivan@Ivans-Air.Dlink>
2022-12-28 22:10:46 +0300

    added calc

---
+++ src/sborka/CMakeFiles/calc_view.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ivan/Projects/CPP/smartcalc/src/Controller/Controller.cc" "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/Controller/Controller.cc.o" "gcc" "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/Controller/Controller.cc.o.d"
+  "/Users/ivan/Projects/CPP/smartcalc/src/Model/Model.cc" "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/Model/Model.cc.o" "gcc" "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/Model/Model.cc.o.d"
+  "/Users/ivan/Projects/CPP/smartcalc/src/View/mainwindow.cc" "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/View/mainwindow.cc.o" "gcc" "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/View/mainwindow.cc.o.d"
+  "/Users/ivan/Projects/CPP/smartcalc/src/sborka/calc_view_autogen/mocs_compilation.cpp" "CMakeFiles/calc_view.dir/calc_view_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/calc_view.dir/calc_view_autogen/mocs_compilation.cpp.o.d"
+  "/Users/ivan/Projects/CPP/smartcalc/src/build/main.cc" "CMakeFiles/calc_view.dir/main.cc.o" "gcc" "CMakeFiles/calc_view.dir/main.cc.o.d"
+  "/Users/ivan/Projects/CPP/smartcalc/src/build/qcustomplot.cpp" "CMakeFiles/calc_view.dir/qcustomplot.cpp.o" "gcc" "CMakeFiles/calc_view.dir/qcustomplot.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/sborka/CMakeFiles/calc_view.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc_view_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/calc_view_autogen.dir/ParseCache.txt"
+  "calc_view_autogen"
+  "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/Controller/Controller.cc.o"
+  "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/Controller/Controller.cc.o.d"
+  "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/Model/Model.cc.o"
+  "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/Model/Model.cc.o.d"
+  "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/View/mainwindow.cc.o"
+  "CMakeFiles/calc_view.dir/Users/ivan/Projects/CPP/smartcalc/src/View/mainwindow.cc.o.d"
+  "CMakeFiles/calc_view.dir/calc_view_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/calc_view.dir/calc_view_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/calc_view.dir/main.cc.o"
+  "CMakeFiles/calc_view.dir/main.cc.o.d"
+  "CMakeFiles/calc_view.dir/qcustomplot.cpp.o"
+  "CMakeFiles/calc_view.dir/qcustomplot.cpp.o.d"
+  "calc_view.app/Contents/MacOS/calc_view"
+  "calc_view.app/Contents/Resources/MyIcon.icns"
+  "calc_view.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calc_view.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
